541f111ade76e2c5928a8f04d6330d4ad7059603
riddhi <riddhi@matrixbricks.com>
2025-02-13 17:21:35 +0530

    SHopping guide part B backedn

---
+++ app/Http/Controllers/Backend/ShoppingGuidePartBController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\ShoppingGuidePartB;
+
+use Carbon\Carbon;
+
+class ShoppingGuidePartBController extends Controller
+{
+
+    public function index()
+    {
+        $details = ShoppingGuidePartB::orderBy('id', 'desc')->whereNull('deleted_by')->get();
+        return view('backend.shopping.partB.index', compact('details'));
+    }
+
+    public function create(Request $request)
+    { 
+        return view('backend.shopping.partB.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'banner_heading' => 'nullable|string|max:255',
+            'short_description' => 'nullable|string',
+            'product_image' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
+            'title' => 'required|string|max:255',
+            'detailed_description' => 'required|string',
+        ], [
+            'banner_heading.required' => 'The Section Heading field is required.',
+            'banner_heading.max' => 'The Section Heading cannot exceed 255 characters.',
+        
+            'short_description.required' => 'The Short Description field is required.',
+        
+            'product_image.required' => 'Please upload an image.',
+            'product_image.image' => 'The uploaded file must be an image.',
+            'product_image.mimes' => 'Only JPG, JPEG, PNG, and WEBP formats are allowed.',
+            'product_image.max' => 'The image size must not exceed 2MB.',
+        
+            'title.required' => 'The Title field is required.',
+            'title.max' => 'The Title cannot exceed 255 characters.',
+        
+            'detailed_description.required' => 'The Detailed Description field is required.',
+        ]);
+
+        if ($request->hasFile('product_image')) {
+            $productImage = $request->file('product_image');
+            $productImageName = time() . rand(10, 999) . '.' . $productImage->getClientOriginalExtension();
+            $productImage->move(public_path('uploads/shopping/'), $productImageName);
+        }
+
+        ShoppingGuidePartB::create([
+            'section_heading' => $request->banner_heading,
+            'short_description' => $request->short_description,
+            'image' => $productImageName ?? null,
+            'title' => $request->title,
+            'detailed_description' => $request->detailed_description,
+            'inserted_at' => Carbon::now(),
+            'inserted_by' => Auth::id(),
+        ]);
+
+        return redirect()->route('guide-partB.index')->with('message', 'Details added successfully.');
+    }
+
+    public function edit($id)
+    {
+        $details = ShoppingGuidePartB::findOrFail($id);
+        return view('backend.shopping.partB.edit', compact('details'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'banner_heading' => 'nullable|string|max:255',
+            'short_description' => 'nullable|string',
+            'product_image' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:2048',
+            'title' => 'required|string|max:255',
+            'detailed_description' => 'required|string',
+        ], [
+            'banner_heading.max' => 'The Section Heading cannot exceed 255 characters.',
+
+            'product_image.image' => 'The uploaded file must be an image.',
+            'product_image.mimes' => 'Only JPG, JPEG, PNG, and WEBP formats are allowed.',
+            'product_image.max' => 'The image size must not exceed 2MB.',
+
+            'title.required' => 'The Title field is required.',
+            'title.max' => 'The Title cannot exceed 255 characters.',
+
+            'detailed_description.required' => 'The Detailed Description field is required.',
+        ]);
+
+        $details = ShoppingGuidePartB::findOrFail($id);
+
+        if ($request->hasFile('product_image')) {
+            
+            $productImage = $request->file('product_image');
+            $productImageName = time() . rand(10, 999) . '.' . $productImage->getClientOriginalExtension();
+            $productImage->move(public_path('uploads/shopping/'), $productImageName);
+        } else {
+            $productImageName = $details->image; 
+        }
+
+        $details->update([
+            'section_heading' => $request->banner_heading,
+            'short_description' => $request->short_description,
+            'image' => $productImageName,
+            'title' => $request->title,
+            'detailed_description' => $request->detailed_description,
+            'modified_at' => Carbon::now(),
+            'modified_by' => Auth::id(),
+        ]);
+
+        return redirect()->route('guide-partB.index')->with('message', 'Details updated successfully.');
+    }
+
+    public function destroy(string $id)
+    {
+        $data['deleted_by'] =  Auth::id();
+        $data['deleted_at'] =  Carbon::now();
+        try {
+            $industries = ShoppingGuidePartB::findOrFail($id);
+            $industries->update($data);
+
+            return redirect()->route('guide-partB.index')->with('message', 'Details deleted successfully!');
+        } catch (Exception $ex) {
+            return redirect()->back()->with('error', 'Something Went Wrong - ' . $ex->getMessage());
+        }
+    }
+
+}
---
+++ resources/views/backend/shopping/partB/edit.blade.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html lang="en">
+    
+<head>
+    @include('components.backend.head')
+</head>
+	   
+		@include('components.backend.header')
+
+	    <!--start sidebar wrapper-->	
+	    @include('components.backend.sidebar')
+	   <!--end sidebar wrapper-->
+
+
+        <div class="page-body">
+          <div class="container-fluid">
+            <div class="page-title">
+              <div class="row">
+                <div class="col-6">
+                  <h4>Add Guide Part B Form</h4>
+                </div>
+                <div class="col-6">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                    <a href="{{ route('guide-partB.index') }}">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active">Add Details</li>
+                </ol>
+
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Container-fluid starts-->
+          <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                    <div class="card-header">
+                        <h4>Guide Part B Form</h4>
+                        <p class="f-m-light mt-1">Fill up your true details and submit the form.</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="vertical-main-wizard">
+                        <div class="row g-3">    
+                            <!-- Removed empty col div -->
+                            <div class="col-12">
+                            <div class="tab-content" id="wizard-tabContent">
+                                <div class="tab-pane fade show active" id="wizard-contact" role="tabpanel" aria-labelledby="wizard-contact-tab">
+                                <form class="row g-3 needs-validation custom-input" novalidate 
+                                    action="{{ route('guide-partB.update', $details->id) }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+
+                                    <!-- Section Heading -->
+                                    <div class="col-12">
+                                        <label class="form-label" for="banner_heading">Section Heading</label>
+                                        <input type="text" class="form-control" id="banner_heading" name="banner_heading" 
+                                            placeholder="Enter Section Heading" value="{{ old('section_heading', $details->section_heading) }}">
+                                        <div class="invalid-feedback">Please enter a Section heading.</div>
+                                    </div>
+
+                                    <!-- Short Description with Summernote -->
+                                    <div class="col-12 mb-5">
+                                        <label class="form-label" for="short_description">Short Description</label>
+                                        <textarea class="form-control summernote" id="short_description" name="short_description" 
+                                                placeholder="Enter Short Description">{{ old('short_description', $details->short_description) }}</textarea>
+                                        <div class="invalid-feedback">Please enter a Short description.</div>
+                                    </div><br><br><br><br><br><br>
+
+                                    <!-- Image Upload -->
+                                    <div class="col-12">
+                                        <label class="form-label" for="product_image">Image <span class="txt-danger">*</span></label>
+                                        <input type="file" class="form-control" id="product_image" name="product_image" accept="image/*" 
+                                            onchange="previewImage(this, 'image_preview')">
+                                        <small class="text-secondary"><b>Note: The file size should be less than 2MB.</b></small><br>
+                                        <small class="text-secondary"><b>Note: Only files in .jpg, .jpeg, .png, .webp format can be uploaded.</b></small>
+                                        <div id="image_preview" class="mt-2">
+                                            @if ($details->image)
+                                                <img src="{{ asset('uploads/shopping/' . $details->image) }}" alt="Uploaded Image" width="100">
+                                            @endif
+                                        </div>
+                                        <div class="invalid-feedback">Please upload an image.</div>
+                                    </div>
+
+                                    <!-- Title -->
+                                    <div class="col-12">
+                                        <label class="form-label" for="title">Title <span class="txt-danger">*</span></label>
+                                        <input type="text" class="form-control" id="title" name="title" placeholder="Enter Title" 
+                                            value="{{ old('title', $details->title) }}" required>
+                                        <div class="invalid-feedback">Please enter a Title.</div>
+                                    </div>
+
+                                    <!-- Detailed Description with Summernote -->
+                                    <div class="col-12 mb-5">
+                                        <label class="form-label" for="detailed_description">Detailed Description <span class="txt-danger">*</span></label>
+                                        <textarea class="form-control summernote" id="summernote" name="detailed_description" 
+                                                placeholder="Enter Detailed Description" required>{{ old('detailed_description', $details->detailed_description) }}</textarea>
+                                        <div class="invalid-feedback">Please enter a detailed description.</div>
+                                    </div><br><br><br><br><br><br>
+
+                                    <!-- Form Actions -->
+                                    <div class="col-12 text-end">
+                                        <a href="{{ route('guide-partB.index') }}" class="btn btn-danger px-4">Cancel</a>
+                                        <button class="btn btn-primary" type="submit">Update</button>
+                                    </div>
+                                </form>
+
+
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+          </div>
+        </div>
+        <!-- footer start-->
+        @include('components.backend.footer')
+        </div>
+        </div>
+
+
+       
+       @include('components.backend.main-js')
+
+<script>
+    function previewImage(input, previewId) {
+        var preview = document.getElementById(previewId);
+        preview.innerHTML = ''; // Clear previous preview
+
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var img = document.createElement("img");
+                img.setAttribute("src", e.target.result);
+                img.setAttribute("alt", "Image Preview");
+                img.setAttribute("width", "100");
+                preview.appendChild(img);
+            };
+            reader.readAsDataURL(input.files[0]); // Read file as Data URL
+        }
+    }
+</script>
+
+
+
+
+
+</body>
+
+</html>
